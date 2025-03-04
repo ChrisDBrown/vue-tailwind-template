9c6141c15012630d97f15e48788f0f97518cf01a
ChrisDBrown <owner@chrisdbrown.co.uk>
2025-03-04 16:04:55 +0000

    feat: Upgrade to tailwindcss 4

--- tests/components/HelloWorld.spec.ts
+++ tests/components/HelloWorld.spec.ts
@@ -8,7 +8,7 @@ describe("HelloWorld", () => {
   it("renders properly", () => {
     const wrapper = mount(HelloWorld, {
       props: { msg: "Hello Vitest" },
-      global: { plugins: [createTestingPinia({ createSpy: vi.fn() })] },
+      global: { plugins: [createTestingPinia({ createSpy: vi.fn })] },
     });
     expect(wrapper.text()).toContain("Hello Vitest");
   });
